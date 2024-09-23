124e99c7fffccfd08b76d06374b01db7444566ec
Jared Spaulding <jspaulding@spauldoindustries.com>
2024-09-22 19:03:55 -0600

    client setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@assets": path.resolve(__dirname, "./src/assets"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@layouts": path.resolve(__dirname, "./src/layouts"),
+            "@services": path.resolve(__dirname, "./src/services"),
+            "@utils": path.resolve(__dirname, "./src/utils"),
+        },
+    },
+});
